a8dc410ec25c94c25a1af72fd952f31c810c1031
odilov2 <jfjksldjsjhjdl@gmail.com>
2024-04-25 23:47:52 +0500

    change ip

--- project/settings.py
+++ project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["178.128.41.226"]
+ALLOWED_HOSTS = ["161.35.36.37"]
 
 
 # Application definition
@@ -115,10 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 #markdown
 
 
